2dc984aae51a76013ccd61c3e98a2c1657958f7d
Derek Scruggs <me@derekscruggs.com>
2023-12-09 08:14:52 -0600

    added comment

--- src/lib/authutils.tsx
+++ src/lib/authutils.tsx
@@ -2,12 +2,14 @@
 import { Navigate } from 'react-router-dom';
 import { authenticated } from './authcontext';
 
+
 export function withAuth(Component: React.ComponentType<any>): React.FC<any> {
   const WithAuth: React.FC<any> = (props) => {
     const isAuthenticated = authenticated();
     if (!isAuthenticated) {
-      // Redirect to the login page if not authenticated
+      //capture the current path in localStorage to redirect to after auth
       localStorage.setItem('redirect',document.location.pathname)
+      // Redirect to the login page if not authenticated
       return <Navigate to="/login" replace />;
     }
 
