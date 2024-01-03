054f27af00b63f24372f25279749b766bf8d6eda
Derek Scruggs <me@derekscruggs.com>
2024-01-02 20:56:51 -0600

    improvement to redirect to login if no auth

--- src/lib/authutils.tsx
+++ src/lib/authutils.tsx
@@ -1,22 +1,29 @@
 // authUtils.tsx
 import { Navigate } from 'react-router-dom';
 import {authenticated} from '../lib/authcontext'
-import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function withAuth(Component: React.ComponentType<any>): React.FC<any> {
   const WithAuth: React.FC<any> = (props) => {
     const isAuthenticated = authenticated();
-    console.log('auth context is', isAuthenticated)
+    const navigate = useNavigate()
     
     if (!isAuthenticated) {
       //capture the current path in localStorage to redirect to after auth
       localStorage.setItem('redirect',document.location.pathname)
       // Redirect to the login page if not authenticated
-      return <Navigate to="/login" replace />;
+      const relogin = true;
+      return <Navigate to="/" replace={true} state={{ relogin }} />;
+      
     }
 
     // Render the component if authenticated
-    return <Component {...props} />;
+    return (
+            <>
+            {isAuthenticated &&
+            <Component {...props} />
+            }
+            </>)
   };
 
   return WithAuth;
