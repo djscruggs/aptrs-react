70dd70bbd162337b404720cb3e1819ad71c063d7
Derek Scruggs <me@derekscruggs.com>
2024-01-14 17:40:46 -0600

    fix to colors in vulnerability form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  /* These text colors are generated dynamically in vulnerabilities
+     Tailwindcss can't create them at compile time so we explicitly add them here. */
+  safelist: [
+    'text-[#3399FF]',
+    'text-[#FF491C]',
+    'text-[#F66E09]',
+    'text-[#FBBC02]',
+    'text-[#20B803]'
+  ],
   theme: {
     extend: {},
   },
