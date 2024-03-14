c321068bb6ee79cb8e7f9c4b9914b939f5a17d0f
Derek Scruggs <me@derekscruggs.com>
2024-03-13 20:59:49 -0500

    fixed app component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,17 @@
-import App from './App'
 import { render, screen } from './lib/test-utils'
 import { beforeAll, describe, expect, expectTypeOf, test } from 'vitest';
+import App from './App'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event'
 
 describe('Simple working test', () => {
   test('the home  page renders', () => {
-    render(<App />)
+    const element = 
+        <BrowserRouter >
+            <App />
+        </BrowserRouter>
+    render(element)
     expect(screen.getByText(/Welcome!/i)).toBeInTheDocument()
   })
 
