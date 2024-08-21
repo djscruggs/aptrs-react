d788040878513d2470e48345f89f5d09a2c7d50c
Derek Scruggs <me@derekscruggs.com>
2024-08-21 16:45:24 -0500

    fix to layout so avatar is not fixed

--- src/components/themeIcon.tsx
+++ src/components/themeIcon.tsx
@@ -1,13 +1,13 @@
 
-import { useContext, useState, createContext } from 'react';
 import { MoonIcon, SunIcon} from '@heroicons/react/24/solid'
 
 interface ThemeProps {
   size?: string
   theme: string
+  className?: string
   toggleTheme: () => void
 }
-export function ThemeIcon({size, theme, toggleTheme}: ThemeProps) {
+export function ThemeIcon({size, theme, toggleTheme, className}: ThemeProps) {
   if(!size) size = 'sm'
   const sizes: Record<string, string> = {
     'sm': 'w-8 h-8',
@@ -19,7 +19,7 @@ export function ThemeIcon({size, theme, toggleTheme}: ThemeProps) {
   
   return (
     <>
-    <div className='cursor-pointer' onClick={toggleTheme}>
+    <div className={`cursor-pointer ${className}`} onClick={toggleTheme}>
     {theme === 'light' ? <SunIcon className={sizes[size]} /> : <MoonIcon className={sizes[size]} />}
     </div>
     </>
